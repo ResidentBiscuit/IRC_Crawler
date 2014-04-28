41fc81067acc8a94a5788a6d84c14f2ed3a496ac
ResidentBiscuit <jared.ready1@gmail.com>
2014-04-28 13:44:41 -0500

    Add key, change to serial

--- scripts/sql/user.sql
+++ scripts/sql/user.sql
@@ -1,7 +1,13 @@
 -- Creates user_name table --
 
 CREATE TABLE user_name(
+	user_id		serial			NOT NULL,
+	network_id	int				NOT NULL,
 	user_name	varchar(100)	NOT NULL,
-	real_name	varchar(100) NULL,
-	PRIMARY KEY(user_name)
+	real_name	varchar(100) 	NULL,
+	PRIMARY KEY(user_id, network_id),
+	FOREIGN KEY(network_id)
+		REFERENCES irc_network(network_id)
+			ON UPDATE NO ACTION
+			ON DELETE NO ACTION
 	);
