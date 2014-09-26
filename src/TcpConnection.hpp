816d8140cae7e2e6875cc09e8477ba5cd63c2aec
ResidentBiscuit <jared.ready1@gmail.com>
2014-09-26 15:54:41 -0500

    More doxygen additions

--- src/TcpConnection.hpp
+++ src/TcpConnection.hpp
@@ -20,20 +20,36 @@ class TcpConnection
 {
 public:
     /**
-     * Constructor.
-     * @param host 
+     * Constructor
+     * @param host URL or IPv4 or v6 address of target host
+     * @param port port number
      */
     TcpConnection(const std::string& host, const std::string& port);
+    /**
+     * Open a connection to host:port
+     */
     void connect();
+    /**
+     * Close connection
+     */
     void disconnect();
-    void read_handler(const boost::system::error_code& error, std::size_t bytes_transferred);
-    void write_handler(const boost::system::error_code& error, std::size_t bytes_transferred);
+    /**
+     * Send a message though the connection
+     * @param message message to send
+     */
     void send(const std::string& message);
+    /**
+     * Messages are pushed onto a queue asynchronously. This method pops a message off the queue.
+     * @return the message.
+     */
     const std::string& get_next_message();
     bool has_message();
     bool is_connected();
  
 private:
+    void read_handler(const boost::system::error_code& error, std::size_t bytes_transferred);
+    void write_handler(const boost::system::error_code& error, std::size_t bytes_transferred);
+
     std::string m_host;
     std::string m_port;
     std::queue<std::string> m_recv_queue;
